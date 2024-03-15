326d0e9538b47a12d662a462e88fb808e549964d
kenken <43767445+tkhs0604@users.noreply.github.com>
2024-03-16 01:33:24 +0900

    Update UI to use SharingTextBroadcastReceiver

--- app/src/main/java/com/tkhs0604/broadcastreceiversample/MainActivity.kt
+++ app/src/main/java/com/tkhs0604/broadcastreceiversample/MainActivity.kt
@@ -3,41 +3,75 @@ package com.tkhs0604.broadcastreceiversample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tkhs0604.broadcastreceiversample.ui.theme.BroadcastReceiverSampleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val broadcastReceiver = SharingTextBroadcastReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        broadcastReceiver.register(this)
+
         setContent {
             BroadcastReceiverSampleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        var text by rememberSaveable { mutableStateOf("") }
+                        TextField(
+                            value = text,
+                            onValueChange = { text = it }
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Button(
+                            onClick = {
+                                runCatching {
+                                    val intent = SharingTextBroadcastReceiver.createChooserIntent(
+                                        this@MainActivity,
+                                        text
+                                    )
+                                    startActivity(intent)
+                                }
+                            },
+                        ) {
+                            Text(text = "share text")
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onDestroy() {
+        broadcastReceiver.unregister(this)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BroadcastReceiverSampleTheme {
-        Greeting("Android")
+        super.onDestroy()
     }
 }
