a58c5922477c79ac6da309e3078a47026ae4f179
kenken <43767445+tkhs0604@users.noreply.github.com>
2024-03-16 10:24:05 +0900

    add SharingTextBroadcastReceiverTest (Success)

--- feature_share/build.gradle.kts
+++ feature_share/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.test.ext:junit:1.1.5")
     testImplementation("org.robolectric:robolectric:4.11.1")
+    testImplementation("androidx.test:rules:1.5.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- feature_share/src/test/java/com/tkhs0604/broadcastsample/feature/share/SharingTextBroadcastReceiverTest.kt
+++ feature_share/src/test/java/com/tkhs0604/broadcastsample/feature/share/SharingTextBroadcastReceiverTest.kt
@@ -3,13 +3,20 @@ package com.tkhs0604.broadcastsample.feature.share
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 import com.tkhs0604.broadcastreceiversample.feature.share.SharingTextBroadcastReceiver
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SharingTextBroadcastReceiverTest {
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        "com.tkhs0604.broadcastreceiversample.feature.share.test.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION"
+    )
 
     private val context = ApplicationProvider.getApplicationContext<Context>()
 
